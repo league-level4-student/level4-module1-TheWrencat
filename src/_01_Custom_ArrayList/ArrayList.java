19c6921142c982e937b2c91b43b392c9c5f7860a
League <league@WorthyMini-5.attlocal.net>
2018-09-20 19:14:15 -0700

    finished array list thing

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -1,36 +1,48 @@
 package _01_Custom_ArrayList;
 
+import java.util.Stack;
+
 @SuppressWarnings("unchecked")
 
 public class ArrayList<T> {
 
-	public ArrayList() {
-
-	}
+	Stack<T> stk = new Stack<T>();
+	
 
 	public T get(int loc) throws IndexOutOfBoundsException {
-
-		return null;
+		T thing = stk.get(loc);
+		return thing;
 	}
 
 	public void add(T val) {
-
+		stk.add(val);
+		
 	}
 
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-
+		stk.insertElementAt(val, loc);
 	}
 
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-
+		stk.set(loc, val);
 	}
 
 	public void remove(int loc) throws IndexOutOfBoundsException {
-
+			stk.remove(loc);
 	}
 
 	public boolean contains(T val) {
-
-		return false;
+		boolean con = false;
+		if(stk.contains(val)) {
+			con = true;
+		}
+		return con;
 	}
+
+	public int size() {
+		int count= stk.size();
+		return count;
+	}
+	
+
 }
